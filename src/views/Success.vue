b06d729ca334c3e15388c9a86fcc68136c76c68b
Ankesh9618 <ankeshagarwal2015@gmail.com>
2024-06-26 09:14:07 +0530

    DJackets Version : 2

---
+++ src/views/Success.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="page-success">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Thank You</h1>
+
+                <p>Your order will be processed in 48 hours</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: "Success",
+    data(){
+
+    },
+    mounted(){
+        document.title = "Success || Djackets"
+    },
+}
+</script>
